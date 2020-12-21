b08045b830d2934b0ce2a977e12edf1c88131612
Zahid <zahid@appsgenii.com>
2020-12-21 14:40:34 +0500

    socket.io 2.x version

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+
+var http = require('http').createServer();
 var io = require('socket.io')(http);
 
 var home_servers = {};
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-
 io.on('connection', (socket) => {
     
     socket.on('disconnect', () => {
